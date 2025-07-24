94231b9a53af49a4c99f20d569ed511d47ea0f4c
AtharvaShirke95032 <atharvashirke9503@gmail.com>
2025-07-24 13:45:48 +0530

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComapionSession = () => {
+  return (
+    <div>
+      ComapionSession
+
+    </div>
+  )
+}
+
+export default ComapionSession
---
+++ app/companions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanionsLibrary = () => {
+  return (
+    <div>
+      companionlibrary
+    </div>
+  )
+}
+
+export default CompanionsLibrary
